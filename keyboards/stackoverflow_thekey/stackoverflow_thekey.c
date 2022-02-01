4d4effc5862484d005f49c39551c5efadb5620d9
Silcet <camorga1@gmail.com>
2022-02-01 22:35:36 +0100

    Initial commit of Stack Overflow the key keyboard

---
+++ keyboards/stackoverflow_thekey/stackoverflow_thekey.c
@@ -0,0 +1,4 @@
+// Copyright 2022 Carlos (@Silcet)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "stackoverflow_thekey.h"
---
+++ keyboards/stackoverflow_thekey/stackoverflow_thekey.h
@@ -0,0 +1,20 @@
+// Copyright 2022 Carlos (@Silcet)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+    K00, K01, K02 \
+) { \
+    { K00, K01, K02 }, \
+}
